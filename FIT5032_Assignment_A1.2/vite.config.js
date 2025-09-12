a49b0aa3fd8af4ef527475f97900283a44dde3ca
myao0007 <myao0007@student.monash.edu>
2025-09-12 16:03:09 +1000

    added Content Security Policy in vite.config.js

--- FIT5032_Assignment_A1.2/vite.config.js
+++ FIT5032_Assignment_A1.2/vite.config.js
@@ -13,6 +13,20 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+    }
   },
+  server: {
+    headers: {
+      "Content-Security-Policy":
+        "default-src 'self'; " +
+        "script-src 'self' 'unsafe-eval'; " +   // 开发环境允许 HMR / eval
+        "style-src 'self' 'unsafe-inline'; " +  // 开发环境允许内联样式
+        "img-src 'self' data: https:; " +
+        "font-src 'self' https://fonts.gstatic.com; " +
+        "connect-src 'self' ws: wss: https://firestore.googleapis.com https://www.googleapis.com https://securetoken.googleapis.com https://identitytoolkit.googleapis.com https://firebasestorage.googleapis.com; " +
+        "media-src 'self' https://firebasestorage.googleapis.com; " +
+        "frame-ancestors 'self'; " +
+        "object-src 'none'; base-uri 'self';"
+    }
+  }
 })
